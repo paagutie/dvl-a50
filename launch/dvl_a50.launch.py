0c0636ea71cfb9d7cd911792e3a2490f738aaecf
pgutierrez <pgutierrez@marum.de>
2022-04-11 16:49:08 +0200

    Executable fixed

--- launch/dvl_a50.launch.py
+++ launch/dvl_a50.launch.py
@@ -8,7 +8,7 @@ def generate_launch_description():
     launch.actions.DeclareLaunchArgument('ip_address', default_value='192.168.194.95'),
     dvl_a50 = launch_ros.actions.Node(
         package='dvl_a50', 
-        node_executable='dvl_a50_node', 
+        executable='dvl_a50_sensor', 
         parameters=[{'dvl_ip_address': launch.substitutions.LaunchConfiguration('ip_address')}],
         output='screen')
 
