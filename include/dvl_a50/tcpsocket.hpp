50136149a37f0e826e502c6405cb014e27501445
paagutie <paagutie@gmail.com>
2023-06-06 19:27:45 +0200

    Added method for data sending

--- include/dvl_a50/tcpsocket.hpp
+++ include/dvl_a50/tcpsocket.hpp
@@ -11,6 +11,7 @@
 
 #include <poll.h>
 #include <time.h>
+#include <thread>
 
 #define UNUSED(expr) do { (void)(expr); } while (0)
 
@@ -18,6 +19,7 @@
 class TCPSocket
 {
 public:
+
     TCPSocket(char *address, int port)
     {
         memset(&serv_addr, 0, sizeof(serv_addr));
@@ -43,14 +45,12 @@ public:
 
     int Connect(unsigned int timeout_ms, std::string &error, int &error_code)
     {
-    
-   
         // Convert IPv4 and IPv6 addresses from text to binary form
-	if(inet_pton(AF_INET, this->address, &serv_addr.sin_addr)<=0) 
-	{
+        if(inet_pton(AF_INET, this->address, &serv_addr.sin_addr)<=0) 
+	    {
             error = "Invalid address / Address not supported";
             return -1;
-	}
+        }
 
         
         // Set non-blocking 
@@ -66,54 +66,54 @@ public:
         } 
         
 	
-	if (connect(this->sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-	{	    
-	    // Trying to connect with timeout 
-           if (errno == EINPROGRESS) { 
-               //fprintf(stderr, "EINPROGRESS in connect() - selecting\n"); 
-               do { 
-                  tv = this->TimevalFromMsec(timeout_ms);
-                  //tv.tv_sec = 5; 
-                  //tv.tv_usec = 0; 
-                  FD_ZERO(&myset); 
-                  FD_SET(this->sock, &myset); 
-                  res = select(this->sock+1, NULL, &myset, NULL, &tv); 
-                  if (res < 0 && errno != EINTR) { 
-                      //fprintf(stderr, "Error connecting %d - %s\n", errno, strerror(errno));
-                      error = "Error connecting " + std::to_string(errno) + " - " + strerror(errno);
-                      error_code = errno;
-                      return -1; 
-                  } 
-                  else if (res > 0) { 
-                      // Socket selected for write 
-                      lon = sizeof(int); 
-                      if (getsockopt(this->sock, SOL_SOCKET, SO_ERROR, (void*)(&valopt), &lon) < 0) { 
-                          error = "Error in getsockopt() " + std::to_string(errno) + " - " + strerror(errno); //error 114
-                          error_code = errno;
-                          return -1; 
-                      } 
-                      // Check the value returned... 
-                      if (valopt) { 
-                         error = "Error in delayed connection() " + std::to_string(valopt) + " - " + strerror(valopt);
-                         error_code = valopt;
-                         return -1; 
-                      } 
-                      break; 
-                 } 
-                 else { 
-                     //fprintf(stderr, "Timeout in select() - Cancelling!\n");
-                     error = "Timeout in select() - Cancelling!"; 
-                     error_code = 100;
-                     return -1; 
-                 } 
-              }while (1); 
-          } 
-          else { 
-              error = "Error connecting " + std::to_string(errno) + " - " + strerror(errno);  //error 103
-              error_code = errno;
-              return -1; 
-          } 
-       } 
+        if (connect(this->sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+        {	    
+            // Trying to connect with timeout 
+            if (errno == EINPROGRESS) { 
+                //fprintf(stderr, "EINPROGRESS in connect() - selecting\n"); 
+                do { 
+                    tv = this->TimevalFromMsec(timeout_ms);
+                    //tv.tv_sec = 5; 
+                    //tv.tv_usec = 0; 
+                    FD_ZERO(&myset); 
+                    FD_SET(this->sock, &myset); 
+                    res = select(this->sock+1, NULL, &myset, NULL, &tv); 
+                    if (res < 0 && errno != EINTR) { 
+                        //fprintf(stderr, "Error connecting %d - %s\n", errno, strerror(errno));
+                        error = "Error connecting " + std::to_string(errno) + " - " + strerror(errno);
+                        error_code = errno;
+                        return -1; 
+                    } 
+                    else if (res > 0) { 
+                        // Socket selected for write 
+                        lon = sizeof(int); 
+                        if (getsockopt(this->sock, SOL_SOCKET, SO_ERROR, (void*)(&valopt), &lon) < 0) { 
+                            error = "Error in getsockopt() " + std::to_string(errno) + " - " + strerror(errno); //error 114
+                            error_code = errno;
+                            return -1; 
+                        } 
+                        // Check the value returned... 
+                        if (valopt) { 
+                            error = "Error in delayed connection() " + std::to_string(valopt) + " - " + strerror(valopt);
+                            error_code = valopt;
+                            return -1; 
+                        } 
+                        break; 
+                    } 
+                   else { 
+                        //fprintf(stderr, "Timeout in select() - Cancelling!\n");
+                        error = "Timeout in select() - Cancelling!"; 
+                        error_code = 100;
+                        return -1; 
+                    } 
+                }while (1); 
+            } 
+            else { 
+                error = "Error connecting " + std::to_string(errno) + " - " + strerror(errno);  //error 103
+                error_code = errno;
+                return -1; 
+            } 
+        } 
        
        // Set to blocking mode again... 
        if( (arg = fcntl(this->sock, F_GETFL, NULL)) < 0) { 
@@ -130,10 +130,14 @@ public:
     }
    
 
-    void Receive(char *tempBuffer)
+    int Receive(char *tempBuffer)
     {
-        int messageLength = recv(this->sock, tempBuffer, buffer_size, 0);
-        UNUSED(messageLength);
+        return recv(this->sock, tempBuffer, buffer_size, 0);
+    }
+
+    void Send(char *tempBuffer)
+    {
+        send(this->sock, tempBuffer, strlen(tempBuffer), 0);
     }
     
     void Close(void)
@@ -143,8 +147,8 @@ public:
 
  
     bool SetRcvTimeout(unsigned int msec_timeout) {
-	struct timeval t = this->TimevalFromMsec(msec_timeout);
-	return this->SetRcvTimeout(t);
+        struct timeval t = this->TimevalFromMsec(msec_timeout);
+        return this->SetRcvTimeout(t);
     }
 
 
@@ -160,17 +164,15 @@ private:
     int buffer_size;
     //sockaddr_in address;
     struct sockaddr_in serv_addr;
- 
+
 
     bool SetRcvTimeout(struct timeval timeout) {
-	int iErr;
-	iErr = setsockopt(this->sock, SOL_SOCKET, SO_RCVTIMEO, (char*) &timeout, sizeof(struct timeval));
-	if (iErr < 0) {
+        int iErr = setsockopt(this->sock, SOL_SOCKET, SO_RCVTIMEO, (char*) &timeout, sizeof(struct timeval));
+        if (iErr < 0) {
             close(sock);
-	    return false;
-	}
-
-	return true;
+            return false;
+        }
+        return true;
     }
 
     struct timeval TimevalFromMsec(unsigned int time_msec){
@@ -181,7 +183,7 @@ private:
 
        return t;
     }
-    
+
 };
 
 #endif
